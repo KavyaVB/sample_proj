e22709283624cd9eded7bb86d4a6843faddf495d
Kavya VB <kavyavb620@gmail.com>
2026-06-12 15:43:38 +0530

    Update and rename server.c to pal.c

---
+++ pal.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int num, originalNum, remainder;
+    int reversedNum = 0;
+
+    // Request input from the user
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    // Save the original number since 'num' will be modified in the loop
+    originalNum = num;
+
+    // Mathematical logic to reverse the integer
+    while (num != 0) {
+        remainder = num % 10;                  // Extract the last digit
+        reversedNum = reversedNum * 10 + remainder; // Append digit to reversed number
+        num /= 10;                             // Remove the last digit from original
+    }
+
+    // Determine if the number is a palindrome
+    if (originalNum == reversedNum) {
+        printf("%d is a palindrome number.\n", originalNum);
+    } else {
+        printf("%d is not a palindrome number.\n", originalNum);
+    }
+
+    return 0;
+}
